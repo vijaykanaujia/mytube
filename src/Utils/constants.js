5b76828627488715ca30fa7d06513479ae9683bc
vijay kanaujia <vijaykanaujia3@gmail.com>
2023-07-25 16:41:48 +0530

    search funtionality done

--- src/Utils/constants.js
+++ src/Utils/constants.js
@@ -2,3 +2,6 @@ const YOUTUBE_API_KEY = "AIzaSyCn0nAidCMlxvA1UnXjGgzazuqxZ3MBfV0";
 export const YOUTUBE_VIDEO_API =
   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=IN&maxResults=50&key=" +
   YOUTUBE_API_KEY;
+
+export const YOUTUBE_SEARCH_API =
+  "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
--- src/components/Head.js
+++ src/components/Head.js
@@ -1,18 +1,47 @@
 import { faUser } from "@fortawesome/free-regular-svg-icons";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faSearch } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
-import { useDispatch } from "react-redux";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { toggleMenu } from "../Utils/appSlice";
+import { YOUTUBE_SEARCH_API } from "../Utils/constants";
+import { cacheResults } from "../Utils/searchSlice";
 
 const Head = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
+  const [showSuggestion, setShowSuggestion] = useState(false);
   const dispatch = useDispatch();
   const toggleMenuHandler = () => {
     dispatch(toggleMenu());
   };
+  const searchCache = useSelector((store) => store.search);
+  useEffect(() => {
+    const searchTimer = setTimeout(() => {
+      if (searchCache[searchQuery]) {
+        setSuggestions(searchCache[searchQuery]);
+      } else {
+        getSearchSuggestions(searchQuery);
+      }
+    }, 200);
+    return () => {
+      clearTimeout(searchTimer);
+    };
+  }, [searchQuery]);
+
+  const getSearchSuggestions = async (query) => {
+    const data = await fetch(YOUTUBE_SEARCH_API + query);
+    const json = await data.json();
+    setSuggestions(json[1]);
+    dispatch(
+      cacheResults({
+        [query]: json[1],
+      })
+    );
+  };
   return (
     <div className="grid grid-flow-col">
-      <div className="flex col-span-1">
+      <div className="flex col-span-3">
         <button
           onClick={() => toggleMenuHandler()}
           className="py-4 pe-4 cursor-pointer"
@@ -27,16 +56,35 @@ const Head = () => {
           />
         </a>
       </div>
-      <div className="p-4 col-span-10 text-center">
-        <input
-          type="text"
-          className="w-1/2 border-solid border-2 py-2 rounded-l-full"
-        />
-        <button className="border-solid border-2 border-blue-200 rounded-r-full p-2">
-          Search
-        </button>
+      <div className="p-4 col-span-6 text-center">
+        <div className="w-full">
+          <input
+            type="text"
+            className="w-4/5 border-solid border-2 p-2 rounded-l-full"
+            onChange={(e) => setSearchQuery(e.target.value)}
+            onFocus={() => setShowSuggestion(true)}
+            onBlur={() => setShowSuggestion(false)}
+          />
+          <button className="border-solid border-2 border-blue-200 rounded-r-full py-2 px-4">
+            <FontAwesomeIcon icon={faSearch} />
+          </button>
+        </div>
+        {showSuggestion && (
+          <div className="fixed text-left bg-white py-2 px-3 mx-10 w-2/5 shadow-xl rounded-sm border-gray-400">
+            <ul>
+              {suggestions.map((suggestion) => {
+                return (
+                  <li>
+                    <FontAwesomeIcon icon={faSearch} className="px-1" />
+                    {suggestion}
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        )}
       </div>
-      <div className="ms-auto col-span-1">
+      <div className="ms-auto col-span-3">
         <button className="p-4">
           <FontAwesomeIcon icon={faUser} />
         </button>
